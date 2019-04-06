6ab1df5bd103ecd6a432f0d9eb65af45ae7eec04
AndrewHinson <hinsonan@gmail.com>
2019-04-05 21:22:31 -0500

    progress

--- js/site.js
+++ js/site.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, {});
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
